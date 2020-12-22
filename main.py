2a36e18c8ff6ad713b3daf77eb4dd0da0dbc5d5d
Anton Polyakov <polyakov.anton@ukr.net>
2020-12-22 20:20:48 +0200

    Add pre deploy setup

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from shop.bot.shop_bot import bot
+from shop.bot.server_bot import bot, app
+import time
+from shop.bot import config
 
-bot.polling()
+bot.remove_webhook()
+time.sleep(0.5)
+bot.set_webhook(config.WEBHOOK_URL, certificate=open('webhook_cert.pem'))
+app.run()
---
+++ shop/bot/server_bot.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort
+from mongoengine import NotUniqueError
+from telebot.types import Update, ReplyKeyboardMarkup, KeyboardButton
+import config
+from telebot import TeleBot
+import time
+
+from shop.bot import constants
+from shop.models.shop_models import User
+
+app = Flask(__name__)
+bot = TeleBot(config.TOKEN)
+
+
+@app.route(config.WEBHOOK_URI, methods=['POST'])
+def handle_webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data()
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    abort(403)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    name = f', {message.from_user.first_name}' if getattr(message.from_user, 'first_name') else ''
+    greetings = constants.GREETINGS.format(name)
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [KeyboardButton(n) for n in constants.START_KB.values()]
+    kb.add(*buttons)
+    bot.send_message(message.chat.id, greetings, reply_markup=kb)
+
+
+
